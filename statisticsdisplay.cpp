420ab3d86c8ad1c4a267b96ec875ed686d4c54b3
Тимофеев Антон Павлович <a.timofeev.2017@stud.nstu.ru>
2020-09-23 13:30:40 +0700

    Сделать экран вывода результатов

--- statisticsdisplay.cpp
+++ statisticsdisplay.cpp
@@ -4,3 +4,16 @@ StatisticsDisplay::StatisticsDisplay()
 {
 
 }
+
+void StatisticsDisplay::update(float t, float h, float p) {
+    this->temperature = t;
+    this->humidity = h;
+    this->pressure = p;
+}
+
+void StatisticsDisplay::display() {
+    cout << "Current conditions:" << endl << "\t"
+         << temperature << "°C" << endl
+         << "%" << humidity << endl
+         << pressure << " mm Hg" << endl;
+}
--- thirdpartydisplay.cpp
+++ thirdpartydisplay.cpp
@@ -4,3 +4,16 @@ ThirdPartyDisplay::ThirdPartyDisplay()
 {
 
 }
+
+void ThirdPartyDisplay::update(float t, float h, float p) {
+    this->temperature = t;
+    this->humidity = h;
+    this->pressure = p;
+}
+
+void ThirdPartyDisplay::display() {
+    cout << "Current conditions:" << endl << "\t"
+         << temperature << "°C" << endl
+         << "%" << humidity << endl
+         << pressure << " mm Hg" << endl;
+}
